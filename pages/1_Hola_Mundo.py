4416b726d242aa61233376c8973e2f80ddc169df
Santiago-Poggio <129192655+Santiago-Poggio@users.noreply.github.com>
2023-09-28 17:17:37 -0300

    Update 1_Hola_Mundo.py
    acentos

--- pages/1_Hola_Mundo.py
+++ pages/1_Hola_Mundo.py
@@ -3,13 +3,13 @@ from PIL import Image
 
 st.title("Hola mundo en python")
 
-st.write("escribir hola mundo es comúnmente lo primero que se hace cuando se utiliza un nuevo lenguaje de programación.")
+st.write("Escribir hola mundo es comúnmente lo primero que se hace cuando se utiliza un nuevo lenguaje de programación.")
 
 st.subheader("Escribir Hola Mundo")
 
-st.write("Para escribir hola mundo en python uno tiene que utilizar la función “print”, que nos permite imprimir en la consola. escribimos print y entre paréntesis y comillas al lado de la función, escribimos “Hola Mundo”. se tendría que ver de esta forma. print(“Hola Mundo”) . Al ejecutar el programa(en la pagina web seria apretando en run) veremos que en la consola se escribió hola mundo. ")
+st.write("Para escribir hola mundo en python uno tiene que utilizar la función “print”, que nos permite imprimir en la consola. Escribimos print y entre paréntesis y comillas al lado de la función, escribimos “Hola Mundo”. se tendría que ver de esta forma. print(“Hola Mundo”) . Al ejecutar el programa(en la página web sería apretando en run) veremos que en la consola se escribió hola mundo. ")
 
-st.write("Asi se tendria que ver cuando lo ejecutemos:")
+st.write("Así se tendría que ver cuando lo ejecutemos:")
 
 image = Image.open("hola_mundo.png")
 
