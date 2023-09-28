4a67ead6a1e9e0f842c4ac8b752048e66152ee9d
Santiago-Poggio <129192655+Santiago-Poggio@users.noreply.github.com>
2023-09-28 17:11:54 -0300

    Update programa.py
    acentos

--- programa.py
+++ programa.py
@@ -6,13 +6,13 @@ st.set_page_config(
     page_icon = None
 )
 
-st.title("Prototipo pagina de python")
+st.title("Prototipo página de python")
 
-st.title("¡bienvenido a mi pagina!")
+st.title("¡bienvenido a mi página!")
 
 st.subheader("¿De qué trata esta página?")
 
-st.write("En esta pagina se hablara sobre qué es python, para qué sirve, algunos conceptos basicos y porqué es importante que los chicos de tercero sepan sobre python, ¡pero incluso si no sos de tercero esta pagina te podría interesar!")
+st.write("En esta pagina se hablara sobre qué es python, para qué sirve, algunos conceptos basicos y por qué es importante que los chicos de tercero sepan sobre python, ¡pero incluso si no sos de tercero esta pagina te podría interesar!")
 
 image = Image.open("python.png")
 st.image(image)
@@ -31,10 +31,10 @@ st.image(image3)
 
 st.write("Bueno,¿Porque Python? Elegí Python porque es un lenguaje de programación de código abierto muy versátil que puede ser utilizado para muchas cosas como por ejemplo, aplicaciones, páginas web(De hecho,¡Esta página web fue hecha con python!), ciencia de datos y el machine learning. Además es un lenguaje muy fácil de entender y de escribir para alguien que nunca tuvo programación y es un lenguaje muy utilizado en el mundo laboral.")
 
-st.subheader("Como usar python ")
+st.subheader("Comó usar python ")
 
 st.write("uno puede usar python instalandolo a través de la página oficial de [Python](https://www.python.org/downloads/) , pero también se puede usar a través de esta [página web](https://www.programiz.com/python-programming/online-compiler/.) llamada programiz y sin tener que instalar nada.")
 
-st.subheader("conocimientos basicos en python")
+st.subheader("conocimientos básicos en python")
 
-st.write("En el menú de la izquierda o apretando la flechita arriba a la izquierda en el celular, podran acceder a unos conocimientos basicos de la programacion en python, ademas de algunos video tutoriales si quieren seguir aprendiendo")
+st.write("En el menú de la izquierda o apretando la flechita arriba a la izquierda en el celular, podrán acceder a unos conocimientos basicos de la programacion en python, ademas de algunos video tutoriales si quieren seguir aprendiendo")
